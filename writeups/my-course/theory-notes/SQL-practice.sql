27963e185ba838006b955b99c95b0fb22ebdf79c
parmis <parmisroshankar1@gmail.com>
2026-06-09 07:44:15 +0330

    add some syntax

--- writeups/my-course/theory-notes/SQL-practice.sql
+++ writeups/my-course/theory-notes/SQL-practice.sql
@@ -1,1 +1,11 @@
+--SQL different syntax
 
+--1) Data Definition Language (DDL)
+--CREATE, ALERT, DROP
+
+CREATE TABLE Customers (
+    ID INT,
+    FirstName VARCHAR(255),
+    Age INT,
+    Address VARCHAR
+);
