13d0f307f8665118603fe0cbf7163b5c95f611e2
parmis <parmisroshankar1@gmail.com>
2026-06-09 07:47:41 +0330

    add more syntax examples

--- writeups/my-course/theory-notes/SQL-practice.sql
+++ writeups/my-course/theory-notes/SQL-practice.sql
@@ -9,3 +9,15 @@ CREATE TABLE Customers (
     Age INT,
     Address VARCHAR
 );
+
+--2) Data Query Language (DQL)
+--SELECT, WHERE, ORDER BY
+
+SELECT LastName, Age
+FROM Customers
+WHERE Age > 18
+ORDER BY Age ASC;
+--UNION operator
+SELECT FirstName, LastName FROM Tehran_Customers
+UNION
+SELECT FirstName, LastName FROM Others_Customers;
