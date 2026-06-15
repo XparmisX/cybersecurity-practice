1511c97b4cca6d145f78e681ade4c28b6ba064a0
parmis <parmisroshankar1@gmail.com>
2026-06-15 19:12:30 +0330

    Update Bash-practice

--- writeups/my-course/theory-notes/Bash-practice.sh
+++ writeups/my-course/theory-notes/Bash-practice.sh
@@ -1,1 +1,35 @@
+pwd
+ls
+ls -ahl
+cd dir_name
+mkdir dir_name
 
+touch myfile
+cat myfile
+file myfile
+wc myfile
+
+cp source destination
+cp -r source destination
+mv source destination
+rm myfile
+rm -r dir_name
+
+whoami
+id
+echo $SHELL
+uname
+uname -a
+
+echo "Hello, World!" > var/www/uploads/hello.txt
+
+cat myfile.txt | grep "Hello, World!" | wc -l
+
+echo "First command"; echo "Second command"
+
+ping quera.org & ping juniora.org
+
+ping quera.org -c 10 && echo "Command finished successfully!"
+
+touch `whoami`.txt
+cat $(whoami).txt
