3a0d40d937b2228379acb82e78752e1b1645688b
parmis <parmisroshankar1@gmail.com>
2026-07-06 22:52:53 +0330

    complete problem-14

--- writeups/my-course/labs/problem-14.sql
+++ writeups/my-course/labs/problem-14.sql
@@ -6,3 +6,24 @@
     your 3rd query here --UPDATE
 -- Section4
     your 4th query here --DELETE
+
+
+-- Final Answer :
+-- Section1
+SELECT country, new_cases
+FROM covid_stats
+WHERE new_cases > 10000
+ORDER BY new_cases DESC;
+
+-- Section2
+INSERT INTO covid_stats (date_reported, country_code, country, new_cases, new_deaths)
+VALUES ('2020-08-19', 'IRN', 'ایران', 15000, 50);
+
+-- Section3
+UPDATE covid_stats
+SET new_cases = 16000
+WHERE country = 'ایران' AND date_reported = '2020-08-18';
+
+-- Section4
+DELETE FROM covid_stats
+WHERE new_deaths = 0;
